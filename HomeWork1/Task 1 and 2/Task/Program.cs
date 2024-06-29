cec3a20a3f2207a18ff932fb81af1f2c8c71dae1
fnzrli2011 <fnzrli109@gmail.com>
2024-06-29 17:41:14 +0400

    C# tasks

---
+++ HomeWork1/Task 1 and 2/Task/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1(1):
+            //for(long i = 1; i <= 100; i++)
+            // {
+            //     if (i%15==0)
+            //     {
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            //task 1(2):
+            //for(long i=15;i<=100;i+=15)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+
+            //task 2:
+            //long point = 100;
+            //if(point<65)
+            //{
+            //    Console.WriteLine("Kesildin");
+            //}
+            //else
+            //    if(point>=65 && point<85)
+            //{
+            //    Console.WriteLine("Adi Diplom");
+            //}
+            //else
+            //    if(point>=85 && point<95)
+            //{
+            //    Console.WriteLine("Seref Diplomu");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yuksek Seref Diplomu");
+            //}
+
+
+            
+        }
+    }
+}
